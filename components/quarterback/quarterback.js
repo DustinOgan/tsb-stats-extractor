452e7b8cead53c6f420dc9a7347f5b21bed56ec8
Dustin Ogan <dustinogan@gmail.com>
2021-04-26 12:17:42 -0400

    update average section to handle 0 attempts case

--- components/quarterback/quarterback.js
+++ components/quarterback/quarterback.js
@@ -1,99 +1,104 @@
-async function mapStats(decimalArray){
+async function mapStats(decimalArray) {
     const qbRaw = {
-    passAttemptsBase : decimalArray[0],
-    passCompleteBase : decimalArray[1],
-    passTdBase : decimalArray[2],
-    passIntBase : decimalArray[3], 
-    passYardsBase : decimalArray[4],
-    rushAttempts : decimalArray[5],
-    rushYardsBase : decimalArray[6],
-    modPassRushYards: decimalArray[7],
-    rushTDsBase: decimalArray[8]
+        passAttemptsBase: decimalArray[0],
+        passCompleteBase: decimalArray[1],
+        passTdBase: decimalArray[2],
+        passIntBase: decimalArray[3],
+        passYardsBase: decimalArray[4],
+        rushAttempts: decimalArray[5],
+        rushYardsBase: decimalArray[6],
+        modPassRushYards: decimalArray[7],
+        rushTDsBase: decimalArray[8]
     };
-    
+
     return qbRaw
 }
 
 
-async function calculatePassAttempts(passAttemptsBase, passTdBase){
+async function calculatePassAttempts(passAttemptsBase, passTdBase) {
     const modifier = (passTdBase % 4) * 256
     const passAttempts = passAttemptsBase + modifier
     return passAttempts;
 }
 
-async function calculatePassCompletions(passCompleteBase, passIntBase){
+async function calculatePassCompletions(passCompleteBase, passIntBase) {
     const modifier = (passIntBase % 4) * 256;
     const passCompletions = passCompleteBase + modifier
     return passCompletions;
 }
 
-async function calculatePassYards(passYardsBase, modPassRushYards){
+async function calculatePassYards(passYardsBase, modPassRushYards) {
     const modifier = (modPassRushYards / 8) * 256
     const passYards = passYardsBase + modifier;
     return passYards;
 }
 
-async function calculatePassTds(passTdBase){
-    const actualTouchDowns = Math.floor(passTdBase/4)
+async function calculatePassTds(passTdBase) {
+    const actualTouchDowns = Math.floor(passTdBase / 4)
     return actualTouchDowns;
 }
 
-async function calculatePassInts(passIntBase){
-    const actualInts = Math.floor(passIntBase/4);
+async function calculatePassInts(passIntBase) {
+    const actualInts = Math.floor(passIntBase / 4);
     return actualInts;
 }
 
-async function calculateRating(yards, attempts, completions, touchdowns, interceptions){
-    const factorA = (((completions/attempts) * 100) -30) /20
-    const factorB = ((touchdowns/attempts)* 100 ) / 5
-    const factorC = (9.5 - ((interceptions/attempts)*100)) /4
-    const factorD = ((yards/attempts) -3) /4
+async function calculateRating(yards, attempts, completions, touchdowns, interceptions) {
+    const factorA = (((completions / attempts) * 100) - 30) / 20
+    const factorB = ((touchdowns / attempts) * 100) / 5
+    const factorC = (9.5 - ((interceptions / attempts) * 100)) / 4
+    const factorD = ((yards / attempts) - 3) / 4
     let rating = (factorA + factorB + factorC + factorD) / .06
-    rating -= rating%.1
+    rating -= rating % .1
     return rating;
 }
 
-async function calculateCompletionPercentage(attempts, completions){
-    let completionPercentage = completions/attempts;
+async function calculateCompletionPercentage(attempts, completions) {
+    let completionPercentage = completions / attempts;
     let completionWholeNumber = completionPercentage * 100
     //hacky truncation without rounding to the tenths place
-    let computedNumber =  (parseInt(completionWholeNumber*10)/10).toFixed(1)  
+    let computedNumber = (parseInt(completionWholeNumber * 10) / 10).toFixed(1)
     return parseFloat(computedNumber);
 }
 
-async function calculateRushAttempts(rushAttempts){
+async function calculateRushAttempts(rushAttempts) {
     return rushAttempts;
 }
 
-async function calculateRushYards(rushYardsBase, modifier){
-    let rushModifier = (modifier % 8) * 256   
+async function calculateRushYards(rushYardsBase, modifier) {
+    let rushModifier = (modifier % 8) * 256
     let totalYards = rushYardsBase + rushModifier
     return totalYards;
 }
 
-async function calculateRushTds(baseRushTds){
+async function calculateRushTds(baseRushTds) {
     let actualRushTds = baseRushTds / 4
     return actualRushTds;
 }
 
-async function calculateAvgYards(yards,attempts){
-    let avg = yards/attempts
-    //hacky truncation without rounding to the tenths place
-    avg =  (parseInt(avg*10)/10).toFixed(1)  
-    return parseFloat(avg)
+async function calculateAvgYards(yards, attempts) {
+    if (attempts == 0) {
+        return 0;
+    } else {
+        let avg = yards / attempts
+        //hacky truncation without rounding to the tenths place
+        avg = (parseInt(avg * 10) / 10).toFixed(1)
+        return parseFloat(avg)
+    }
+
 }
 
-module.exports = { 
-    mapStats : mapStats,
-    calculatePassAttempts : calculatePassAttempts,
-    calculatePassCompletions : calculatePassCompletions,
-    calculatePassYards : calculatePassYards,
-    calculatePassTds : calculatePassTds,
-    calculatePassInts : calculatePassInts,
+module.exports = {
+    mapStats: mapStats,
+    calculatePassAttempts: calculatePassAttempts,
+    calculatePassCompletions: calculatePassCompletions,
+    calculatePassYards: calculatePassYards,
+    calculatePassTds: calculatePassTds,
+    calculatePassInts: calculatePassInts,
     calculateRating: calculateRating,
     calculateCompletionPercentage: calculateCompletionPercentage,
     calculateRushAttempts: calculateRushAttempts,
-    calculateRushYards : calculateRushYards,
-    calculateRushTds : calculateRushTds,
-    calculateAvgYards : calculateAvgYards
+    calculateRushYards: calculateRushYards,
+    calculateRushTds: calculateRushTds,
+    calculateAvgYards: calculateAvgYards
 }
