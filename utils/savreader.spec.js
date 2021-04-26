2f13d5ea2cb00c64f39756269ee02747382392a2
Dustin Ogan <dustinogan@gmail.com>
2021-04-26 11:03:39 -0400

    remove extra lib

--- utils/savreader.spec.js
+++ utils/savreader.spec.js
@@ -1,5 +1,4 @@
 const savReader = require("./savReader")
-const assert = require('chai').assert;
 const expect = require('chai').expect;
 
 describe('savReader', async function(){
@@ -15,7 +14,7 @@ describe('savReader', async function(){
         expect(decimalArray).to.eql([ 16, 155, 125, 108, 194, 0, 0, 112, 0 ])
     })
     it("should just print the decimal array", async function(){
-        const row1 = '10 9B 7D 6C C2 00 00 70 00'
+        const row1 = 'E5 7E 68 50 FC 38 32 59 10'
         const decimalArray = await savReader.hexToDecimal9(row1);
     })
 })
